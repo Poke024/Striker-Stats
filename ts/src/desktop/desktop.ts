5a3d291391eff2eab746cd1ac8e816252f2a5b80
torresHK <adair.tor24@gmail.com>
2023-06-04 23:42:58 -0500

    Tier List Page UI Partial Implementation

--- ts/src/desktop/desktop.ts
+++ ts/src/desktop/desktop.ts
@@ -1,15 +1,17 @@
 import { AppWindow } from "../AppWindow";
 import { kWindowNames } from "../consts";
-
+import * as $ from "jquery";
 // The desktop window is the window displayed while game is not running.
 // In our case, our desktop window has no logic - it only displays static data.
 // Therefore, only the generic AppWindow class is called.
 new AppWindow(kWindowNames.desktop);
 
-var activeNav = (document.getElementById("setBuilder") as HTMLElement);
+var activeNav = (document.getElementById("tierLists") as HTMLElement);
 activeNav.style.background = "rgb(27, 51, 82)";
-var activeTab = (document.getElementById("setBuilderContent") as HTMLElement);
+var activeTab = (document.getElementById("tierListsContent") as HTMLElement);
 activeTab.style.display = "block";
+var activeRole = (document.getElementById("forward") as HTMLElement);
+activeRole.style.background = "rgb(36, 64, 94)";
 
 function changeTab(event) {
     let clickedNav = (event.currentTarget) as HTMLElement;
@@ -22,10 +24,18 @@ function changeTab(event) {
 }
 
 function changeColor(event, rgbString) {
-    let clickedNav = (event.currentTarget) as HTMLElement;
-    if (clickedNav != activeNav) {
-        clickedNav.style.background = rgbString;
+    let clicked = (event.currentTarget) as HTMLElement;
+    if (clicked != activeNav && clicked != activeRole) {
+        clicked.style.background = rgbString;
     }
+}
+
+function changeRole(event) {
+    console.log("Fired changeRole");
+    let clickedRole = (event.currentTarget) as HTMLElement;
+    activeRole.style.background = "rgb(38, 39, 39)";
+    activeRole = clickedRole;
+    activeRole.style.background = "rgb(36, 64, 94)";
 }
 
 for (var tab of Array.from(document.getElementsByClassName("tabLink") as HTMLCollectionOf<HTMLElement>)) {
@@ -37,6 +47,19 @@ for (var tab of Array.from(document.getElementsByClassName("tabLink") as HTMLCol
         changeColor(event, "rgb(49, 53, 89)");
     });
     tab.addEventListener('mousedown', (event) => {
+        changeColor(event, "rgb(38, 39, 39)");
+    });
+}
+
+for (var role of Array.from(document.getElementsByClassName("roleOption") as HTMLCollectionOf<HTMLElement>)) {
+    role.addEventListener('click', changeRole);
+    role.addEventListener('mouseover', (event) => {
+        changeColor(event, "rgb(32, 34, 58)");
+    });
+    role.addEventListener('mouseleave', (event) => {
+        changeColor(event, "rgb(38, 39, 39)");
+    });
+    role.addEventListener('mousedown', (event) => {
         changeColor(event, "rgb(38, 39, 39)");
     });
 }
